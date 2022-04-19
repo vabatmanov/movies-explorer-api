9e085f307f22a69b80a691cf59694b1d74556a71
Batmanov Vlad <v-batmanov@yandex.ru>
2022-04-19 18:57:43 +0700

    added models user,movie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
-const Unauthorized = require('../errors/ErrorUnauthorized');
+const ErrorUnauthorized = require('../errors/ErrorUnauthorized');
 
 module.exports = (req, res, next) => {
   const { NODE_ENV, JWT_SECRET } = process.env;
   if (!req.cookies.jwt) {
-    next(new Unauthorized('Необходима авторизация'));
+    next(new ErrorUnauthorized('Необходима авторизация'));
     return;
   }
   let payload;
   try {
     payload = jwt.verify(req.cookies.jwt, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new Unauthorized('Необходима авторизация'));
+    next(new ErrorUnauthorized('Необходима авторизация'));
     return;
   }
   req.user = payload;
---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+const ErrorUnauthorized = require('../errors/ErrorUnauthorized');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, 'invalid email'],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    default: 'Введите имя пользователя',
+  },
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new ErrorUnauthorized('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new ErrorUnauthorized('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
+
+module.exports = mongoose.model('user', userSchema);
